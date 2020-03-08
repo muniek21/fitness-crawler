0980d7737ee4d501bd86385de13ce51db86a017c
monikaangerhoefer <zaba.mon@gmail.com>
2020-03-08 13:36:36 +0100

    add date to FitnessClass

--- run_crawler.sh
+++ run_crawler.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#
+#while read line
+#  do
+#    scrapy runspider scraper.py -a fitness_club="$line"
+#  done < fitnessClubUrls
 
-while read line
-  do
-    scrapy runspider scraper.py -a fitness_club="$line"
-  done < fitnessClubUrls
+scrapy runspider scraper.py -a fitness_club="POLITECHNIKA https://metropolitechnika.zdrofit.pl/kalendarz-zajec"
--- scraper.py
+++ scraper.py
@@ -1,42 +1,50 @@
 import scrapy
 from enum import Enum
-import sys
+from datetime import datetime, timedelta
+
 
 class Day(Enum):
     MONDAY = 1
     TUESDAY = 2
     WEDNESDAY = 3
     THURSDAY = 4
-    FRIDAY = 5 
+    FRIDAY = 5
     SATURDAY = 6
     SUNDAY = 7
+
 
 class FitnessClass:
     day = Day.MONDAY
     hour = ''
     name = ''
     place = ''
+    date = ''
 
-    def __init__(self, day, hour, name, place):
+    def __init__(self, day, hour, name, place, date):
         self.day = day
         self.hour = hour
         self.name = name
         self.place = place
-    
+        self.date = date
+
     def print_class(self):
-        print(self.name + " " + self.day.name + " " + self.hour + " " + self.place)
+        print(self.name + " " + self.day.name + " " + self.hour + " " + self.place + " " + str(self.date))
+
 
 class FitnessClassScraper(scrapy.Spider):
-    name="fitness_class_spider"
+    name = "fitness_class_spider"
 
     def __init__(self, fitness_club=None, *args, **kwargs):
         super(FitnessClassScraper, self).__init__(*args, **kwargs)
         self.club = fitness_club.split()[0]
-        self.start_urls = [ fitness_club.split()[1] ]
+        self.start_urls = [fitness_club.split()[1]]
 
     def parse(self, response):
         fitness_classes = []
 
+        end_of_the_week = response.css('.week_chooser span::text').extract_first().split('do')[1].strip()
+        sunday = datetime.strptime(end_of_the_week, '%d-%m-%Y')
+        monday = sunday - timedelta(6)
         rows = response.css('table.calendar_table tr')
 
         for row in rows[1:]:
@@ -52,11 +60,8 @@ class FitnessClassScraper(scrapy.Spider):
                 if cell_content is None:
                     continue
                 else:
-                    fitness_classes.append(FitnessClass(Day(num), hour, cell_content, self.club))
+                    date = monday + timedelta(num - 1)
+                    fitness_classes.append(FitnessClass(Day(num), hour, cell_content, self.club, date.date()))
 
         for fitness_class in fitness_classes:
             fitness_class.print_class()
-
-
-
-
